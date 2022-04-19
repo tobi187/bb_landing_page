ea94766aded68881e3f6e2a99e8c86453aa64120
tobi187 <fischerstobi1998@gmail.com>
2022-04-19 16:49:09 +0200

    whatever

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-baby-image h-screen bg-cover bg-no-repeat">
+    <MainNavbar msg="Welcome to Your Vue.js App"/>
+  <div class="w-1/2 mx-20 mt-52">
+    <MainBody />
+  </div>
+    <MainFooter />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainNavbar from './components/Main-Navbar.vue'
+import MainBody from './components/Main-Body.vue'
+import MainFooter from './components/Main-Footer'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainNavbar,
+    MainBody,
+    MainFooter
   }
 }
 </script>
@@ -19,8 +28,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
